53d31c84a19369ceb8fdc185bd119c80e69f66fe
Briand Hui <sora5257@outlook.com>
2026-02-22 11:24:52 +0800

    Refactor file handling and student data operations

--- 10 文件.cpp
+++ 10 文件.cpp
@@ -6,49 +6,50 @@
 #include<stdlib.h>
 
 /*
-�ļ���
+文件：
 
-�ļ����
-������������浽�ڴ�
-�����豸�������壬����������꣬���̣���׼�����豸��
-����������ڴ浽���
-����豸����ӡ������Ļ����׼����豸��
+文件概念：
+输入流：从外存到内存
+输入设备：触摸板，触摸屏，鼠标，键盘（标准输入设备）
+输出流：从内存到外存
+输出设备：打印机，屏幕（标准输出设备）
 
-�ļ����ࣺ
-�ı���ASCII���ļ�����ֱ�Ӳ鿴���ļ��������ı����ַ�������
-�������ļ�������ֱ�Ӳ鿴�����Ƕ���������
+文件分类：
+文本（ASCII）文件：能直接查看的文件，都是文本（字符）数据
+二进制文件：不能直接查看，都是二进制数据
 
-�ļ�������
-�ļ�����ָ�룺FILE * ���ļ������е�Ψһƾ֤
-fopen�����ļ���ע���ļ�·����\��д����\\���ڱ�ʾ\�ַ�����
-fclose���ر��ļ�
-fwrite�����ļ���д���ڴ�����
-fread�����ļ��ж�ȡ���ݵ��ڴ�
-fseek���ı��ļ���дλ�ñ��
+文件操作：
+文件类型指针：FILE * ，文件操作中的唯一凭证
+fopen：打开文件，注意文件路径中\的写法，\\用于表示\字符本身
+fclose：关闭文件
+fwrite：向文件中写入内存数据
+fread：从文件中读取数据到内存
+rewind：将位置指针移动到文件开头
+fseek：将位置指针移动到任意位置，即改变文件读写位置标记
 e.g.
-fseek(fr,0,SEEK_SET);//�Ե�ǰ�ļ���ͷ��Ϊ���յ㣬���λ��Ϊ0������ͷ��
-fseek(fr,-len,SEEK_CUR);//�Ե�ǰ�ļ�λ��ָ����Ϊ���յ㣬���λ��-len��������len���ȣ�
-fseek(fr,-len,SEEK_END);//�Ե�ǰ�ļ�ĩβ��Ϊ���յ㣬���λ��-len��������len���ȣ�
+fseek(fr,0,SEEK_SET);//以当前文件开头作为参照点，相对位置为0（即开头）
+fseek(fr,-len,SEEK_CUR);//以当前文件位置指针作为参照点，相对位置-len（即向左len长度）
+fseek(fr,-len,SEEK_END);//以当前文件末尾作为参照点，相对位置-len（即向左len长度）
 
-�����дԭ����
-�ļ���дλ�ñ�ǣ����ƹ�꣺ͼ�ν����˵������дʱ�Զ��ƶ�
-˳���д
-�����д���ı��ļ���дλ�ñ��
-'\0'���ַ����Ľ�β��ǣ������ļ��в��ɼ�������NUL��ʾ��'\0'�����ļ����Բ���Ҫ��
+随机读写原理：
+文件读写位置标记（类似光标：图形界面的说明）读写时自动移动
+顺序读写
+随机读写：改变文件读写位置标记
+'\0'是字符串的结尾标记，但在文件中不可见或者用NUL表示（'\0'对于文件而言不重要）
 */
 
 #if 0
 int main()
 {
-	FILE* fw = fopen("D:\\Files\\Code\\C_Journey\\C_Journey\\10-Test-01.txt", "w");//����һ���ļ�������·��
+	FILE* fw = fopen("D:\\Files\\Code\\C_Journey\\C_Journey\\10-Test-01.txt", "w");//创建一个文件，绝对路径
 	const char* str = "Hello World!";
-	fwrite(str, sizeof(char), strlen(str), fw);//δд�롰\0��
+	fwrite(str, sizeof(char), strlen(str), fw);//未写入“\0”
 	fclose(fw);
 	return 0;
 }
 #endif
 
-//�Ӽ��������ַ����������������ֱ���û�����һ��"#"Ϊֹ��������ȡ�ַ����������ļ�
+//从键盘输入字符，逐个向磁盘输出（直到用户输入一个"#"为止），并读取字符串、复制文件
 
 #if 0
 int main()
@@ -56,32 +57,32 @@ int main()
 	FILE* fw1 = fopen("D:\\Files\\Code\\C_Journey\\C_Journey\\10-Test-1.txt", "w");
 	if (fw1 == NULL)
 	{
-		printf("���ļ�ʧ�ܣ������ļ�·���Ƿ���ȷ\n");
+		printf("打开文件失败，请检查文件路径是否正确\n");
 		return 0;
 	}
 	char ch1;
 	while ((ch1 = getchar()) != '#')
 	{
 		fwrite(&ch1, sizeof(char), 1, fw1);
-		//fputc(ch, fw);//ֻ������ַ����о�����
+		//fputc(ch, fw);//只能输出字符，有局限性
 	}
 	FILE* fr = fopen("D:\\Files\\Code\\C_Journey\\C_Journey\\10-Test-1.txt", "r");
 	if (fr == NULL)
 	{
-		printf("���ļ�ʧ�ܣ������ļ�·���Ƿ���ȷ\n");
+		printf("打开文件失败，请检查文件路径是否正确\n");
 		return 0;
 	}
-	char buf[100] = "";//��Ŷ�ȡ������
+	char buf[100] = "";//存放读取的数据
 	int len = fread(buf, sizeof(char), 100, fr);
 	printf("%d,%s\n", len, buf);
 	FILE* fw2 = fopen("D:\\Files\\Code\\C_Journey\\C_Journey\\10-Test-2.txt", "w");
 	if (fw2 == NULL)
 	{
-		printf("���ļ�ʧ�ܣ������ļ�·���Ƿ���ȷ\n");
+		printf("打开文件失败，请检查文件路径是否正确\n");
 		return 0;
 	}
 	char ch2;
-	while ((ch2 = fgetc(fr)) != EOF)//��10-Test-1.txt���Ƶ�10-Test-2.txt
+	while ((ch2 = fgetc(fr)) != EOF)//将10-Test-1.txt复制到10-Test-2.txt
 	{
 		fputc(ch2, fw2);
 	}
@@ -92,7 +93,7 @@ int main()
 }
 #endif
 
-//��д���������ݣ���ѡ�����������ѧ�ŵ�ѧ����Ϣ
+//读写二进制数据，并选择性输出奇数学号的学生信息
 
 typedef struct Stub
 {
@@ -104,20 +105,20 @@ typedef struct Stub
 
 void Inputb(Stub* arr, int n)
 {
-	printf("������%d��ѧ����Ϣ��\n", n);
-	printf("���� ѧ�� ���� ��ַ\n");
+	printf("请输入%d个学生信息：\n", n);
+	printf("姓名 学号 年龄 地址\n");
 	for (int i = 0; i < n; i++)
 	{
 		scanf("%s %d %d %s", &arr[i].name, &arr[i].num, &arr[i].age, &arr[i].addr);
 	}
 }
 
-void Saveb(Stub* arr, int n)//b��������
+void Saveb(Stub* arr, int n)//b：二进制
 {
-	FILE* fw = fopen("10-Test-3.txt", "wb");//���·����10-Test-3.txt�͵�ǰԴ�ļ���ͬһ��Ŀ¼���ļ��У�
+	FILE* fw = fopen("10-Test-3.txt", "wb");//相对路径，10-Test-3.txt和当前源文件在同一个目录（文件夹）
 	if (fw == NULL)
 	{
-		printf("���ļ�ʧ�ܣ������ļ�·���Ƿ���ȷ\n");
+		printf("打开文件失败，请检查文件路径是否正确\n");
 		return;
 	}
 	/*for (int i = 0; i < n; i++)
@@ -131,11 +132,11 @@ void Showb()
 	FILE* fr = fopen("10-Test-3.txt", "rb");
 	if (fr == NULL)
 	{
-		printf("���ļ�ʧ�ܣ������ļ�·���Ƿ���ȷ\n");
+		printf("打开文件失败，请检查文件路径是否正确\n");
 		return;
 	}
-	Stub stus;//������ļ���ȡ������
-	while (fread(&stus, sizeof(Stub), 1, fr) > 0)//ע��fread�����ķ���ֵ
+	Stub stus;//保存从文件读取的数据
+	while (fread(&stus, sizeof(Stub), 1, fr) > 0)//注意fread函数的返回值
 	{
 		printf("%s,%d,%d,%s\n", stus.name, stus.num, stus.age, stus.addr);
 	}
@@ -148,8 +149,8 @@ void Last()
 	FILE* fr = fopen("10-Test-3.txt", "rb");
 	if (fr == NULL)
 	{
-		printf("���ļ�ʧ�ܣ������ļ�·���Ƿ���ȷ\n");
-		return;//�������пɷ���-1
+		printf("打开文件失败，请检查文件路径是否正确\n");
+		return;//主函数中可返回-1
 	}
 	Stub stus;
 	int len;
@@ -158,11 +159,11 @@ void Last()
 		len = fread(&stus, sizeof(Stub), 1, fr);
 		if (len < 1)
 		{
-			printf("�ļ�����\n");
+			printf("文件结束\n");
 			break;
 		}
 		printf("%s,%d,%d,%s\n", stus.name, stus.num, stus.age, stus.addr);
-		fseek(fr, sizeof(Stub), SEEK_CUR);//����һ��ѧ��
+		fseek(fr, sizeof(Stub), SEEK_CUR);//跳过一个学生
 	}
 	fclose(fr);
 }
@@ -177,4 +178,5 @@ int main()
 	Showb();
 	return 0;
 }
+
 #endif
